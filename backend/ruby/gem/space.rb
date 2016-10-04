499ddf5556530ff17f83c7c2ea82adf54be2df32
kmbhuvanprasad <bmbhat@qwinix.io>
2016-10-04 18:10:28 +0530

    04/10/16

---
+++ backend/ruby/gem/space.rb
@@ -0,0 +1,13 @@
+def CheckSpace(ste)
+	string = ste.split(' ')
+	cin=string.count
+	puts string
+	puts cin
+	i = 0
+	while i < cin do
+		len = string[i].length
+		puts Len
+		i = i + 1
+	end
+end
+CheckSpace("this is my child")
