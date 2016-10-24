96ae5427a136b75c6f0f76de602121c200bc77f9
kmbhuvanprasad <bmbhat@qwinix.io>
2016-10-24 15:22:42 +0530

    frontend assignment

--- backend/rails/project1/blog/app/models/employee.rb
+++ backend/rails/project1/blog/app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ActiveRecord::Base
+	validates :firstname, presence:true
+	# belongs_to :organization
+	has_many :departments
+	has_many :organizations , :through => :departments
 end
+ 
--- backend/rails/project1/blog/db/schema.rb
+++ backend/rails/project1/blog/db/schema.rb
@@ -11,17 +11,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161020064826) do
+ActiveRecord::Schema.define(version: 20161024072937) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "employees", force: :cascade do |t|
+  create_table "departments", force: :cascade do |t|
+    t.string   "dptname"
+    t.string   "employee_id"
+    t.integer  "organization_id"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  create_table "employees" , force: :cascade do |t|
     t.string   "firstname"
     t.string   "lastname"
     t.string   "employee_code"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "organizations", force: :cascade do |t|
+    t.string   "name"
+    t.string   "city"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
