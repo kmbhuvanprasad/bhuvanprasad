1f24ab513250358ce69f5b6500e34c88338ea2a3
kmbhuvanprasad <bmbhat@qwinix.io>
2016-10-25 17:41:36 +0530

    changes as per 25/10/16

---
+++ backend/rails/practice_proj/myblog/app/helpers/recipes_helper.rb
@@ -0,0 +1,5 @@
+module RecipesHelper
+	def test_helper
+	 return Recipe.last.title
+	end
+end
--- test&assgn/assgnment3/frontend.js
+++ test&assgn/assgnment3/frontend.js
@@ -1,3 +1,28 @@
+// $(document).ready(function(){
+//      $(window).scroll(function () {
+//             if ($(this).scrollTop() > 50) {
+//                 $('#back-to-top').fadeIn();
+//             } else {
+//                 $('#back-to-top').fadeOut();
+//             }
+//         });
+//         // scroll body to 0px on click
+//         $('#back-to-top').click(function () {
+//             $('#back-to-top').tooltip('hide');
+//             $('body,html').animate({
+//                 scrollTop: 0
+//             }, 8000);
+//             return false;
+//         });
+        
+//         $('#back-to-top').tooltip('show');
+
+// });
+
+
+
+
+
 $('.carousel').carousel({
 	interval:15000;
 })
