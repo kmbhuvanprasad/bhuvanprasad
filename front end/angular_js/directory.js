e5d12026487fe1bc145c3c1f3af50a5ecd713150
kmbhuvanprasad <bmbhat@qwinix.io>
2016-11-28 10:26:34 +0530

    28thnov

--- front end/angular_js/directory.js
+++ front end/angular_js/directory.js
@@ -5,16 +5,17 @@
 //        // document.getElementById("inp1").value =document.getElementById("txt1").value;
 //    });
 // });
-
-angular.module('directoryApp',[])
+// flag=0;
+angular.module('directoryApp',['ngAnimate'])
 .controller('directoryController',function(){
 	var dirlist=this;
-	flag=0;
-	dirlist.company =[{name:'Qwinix',age:'90'},
-					 {name:'Infosys',age:'80'},
-					 {name:'thasmai',age:'20'},
-					 {name:'tcs',age:'56'},
-					 {name:'Wipro',age:'45'}
+	dirlist.toggle=false;
+	
+	dirlist.company =[{name:'Qwinix',age:'90',img:'https://s3.amazonaws.com/uifaces/faces/twitter/brad_frost/128.jpg'},
+					 {name:'Infosys',age:'80',img:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'},
+					 {name:'thasmai',age:'20',img:'https://s3.amazonaws.com/uifaces/faces/twitter/sauro/128.jpg'},
+					 {name:'tcs',age:'56',img:'https://s3.amazonaws.com/uifaces/faces/twitter/jsa/128.jpg'},
+					 {name:'Wipro',age:'45',img:'https://s3.amazonaws.com/uifaces/faces/twitter/sauro/128.jpg'}
 					]
 	// dirlist.capture=function(txt){
 	// 	alert('hi this is vilan' +" "+ txt)
@@ -34,18 +35,66 @@ angular.module('directoryApp',[])
 			alert("age should be double digit")
 		}
 	}
-	dirlist.showfunc=function(){
-		flag++;
-		if(flag%2==0)
-		{
-			dirlist.company =[{name:'Qwinix',age:'90'},
-					 {name:'Infosys',age:'80'},
-					 {name:'thasmai',age:'20'},
-					 {name:'tcs',age:'56'},
-					 {name:'Wipro',age:'45'}
-					]
-		}
-		else
-		dirlist.company=null;	
-	}
+	// dirlist.showfunc=function(){
+	// 	flag++;
+	// 	if(flag%2==0)
+	// 	{
+	// 		dirlist.company =[
+	// 				 {name:'Qwinix',age:'90'},
+	// 				 {name:'Infosys',age:'80'},
+	// 				 {name:'thasmai',age:'20'},
+	// 				 {name:'tcs',age:'56'},
+	// 				 {name:'Wipro',age:'45'}
+	// 				]
+	// 	}
+	// 	else
+	// 	{
+	// 		dirlist.company=null;	
+	// 	}
+	// }
+			dirlist.showfunc=function(){
+				dirlist.toggle=!dirlist.toggle;
+			}
+
+				dirlist.flag=true;
+				dirlist.flag1=true;
+				index=0;
+				// debugger;
+				len=dirlist.company.length
+				dirlist.current = dirlist.company[index];
+    		dirlist.next = function()
+    		{
+	        // index=(index+1)%len;
+	        index=index+1;
+	        dirlist.current = dirlist.company[index];
+	        if(index>len-1)
+	        {
+	        	dirlist.flag=false;  	 
+	        }
+	        else
+	        {
+	        	 dirlist.flag=true;
+	        	 dirlist.flag1=true;
+	        }
+	       dirlist.current = dirlist.company[index];
+	        // alert(index)
+	    	}
+	    
+	    	dirlist.prev = function()
+    		{
+	        index=(index-1)%len;
+	        if(index<0)
+	        {
+	        	// index=len-1;
+	        	dirlist.flag1=false;
+	        }
+	        else
+	        {
+	        	 dirlist.flag=true;
+	        	 dirlist.flag1=true;
+	        }
+	        dirlist.current = dirlist.company[index];
+	         // alert(index)
+	      
+	    	}
 });
